24761b157c57a1ca6716d71bed67b8dda6cea29e
Samiksha09Srivastav <samikshasri06@gmail.com>
2024-07-21 10:37:27 +0530

    Added .env

--- src/Firebase.js
+++ src/Firebase.js
@@ -5,14 +5,14 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA6Atc6rJ3AlKCS9tal1_jIJSX6EYFiFjo",
-  authDomain: "invoice-management-c42e0.firebaseapp.com",
-  projectId: "invoice-management-c42e0",
-  storageBucket: "invoice-management-c42e0.appspot.com",
-  messagingSenderId: "498840425422",
-  appId: "1:498840425422:web:791604f3f537f32969119f",
-  measurementId: "G-K4ZSC2QPKJ",
-  databaseURL: ''
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  
 };
 
 // Initialize Firebase
